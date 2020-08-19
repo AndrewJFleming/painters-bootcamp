8c3dffafb940aaaf2809db1a8b6ae72b1198a290
AndrewJFleming <aflemi1@gmail.com>
2020-08-18 21:21:50 -0700

    Updates

---
+++ precache-manifest.d5147fb85e78b013b1fe636e6fe0496a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ab54d05ab40ccadd581840da191144a",
+    "url": "/painters-bootcamp/index.html"
+  },
+  {
+    "revision": "fee268ced5d2a3a66217",
+    "url": "/painters-bootcamp/static/css/2.2d13fb73.chunk.css"
+  },
+  {
+    "revision": "8cb415fc212fc342c004",
+    "url": "/painters-bootcamp/static/css/main.93cc43ed.chunk.css"
+  },
+  {
+    "revision": "fee268ced5d2a3a66217",
+    "url": "/painters-bootcamp/static/js/2.6206bdc0.chunk.js"
+  },
+  {
+    "revision": "066c422d36b6fd7ed07de01853b9bb55",
+    "url": "/painters-bootcamp/static/js/2.6206bdc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cb415fc212fc342c004",
+    "url": "/painters-bootcamp/static/js/main.4ed9a5d5.chunk.js"
+  },
+  {
+    "revision": "c3447e680ecb47712e91",
+    "url": "/painters-bootcamp/static/js/runtime-main.84cd9ffa.js"
+  }
+]);
